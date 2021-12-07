1d243acd1bd2053494fd6225c92e5ed7a0644bd4
Jonathan Lifferth <jonathan.lifferth@gmail.com>
2021-12-07 11:18:22 -0600

    L02 initial commit, swift command line practice

---
+++ Foo.swift
@@ -0,0 +1,5 @@
+func foo() {
+	print("Hello, world")
+}
+
+foo()
---
+++ L02-DataTypes.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+var greeting: String = "Hello, welcome to the playground"
+print(greeting)
+
+var a:Int = 5
+var b:Int = 10
+
+print(a + b + 5)
+
+var c:Float = 2.3
+var d:Double = 13.9
+var e:Bool = true
+
+print(Int(round(d)))
