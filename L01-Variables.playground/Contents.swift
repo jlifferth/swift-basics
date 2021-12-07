cd6b01a7f00f15ab2556b9eeaf7b351fa209aa0c
Jonathan Lifferth <jonathan.lifferth@gmail.com>
2021-12-07 09:45:05 -0600

    L01-Variables initial commit

---
+++ L01-Variables.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+var greeting = "Welcome to the playground, "
+var name = "Juan"
+var greet = greeting + name
+
+print(greet)
+
+var age = 27.0
+var time_left = 47.0
+
+var ageAtDeath = age + time_left
+print("age at death : " + String(ageAtDeath))
+
+print(age + time_left)
+print(age - time_left)
+print(age * time_left)
+print(Float(age / time_left))
